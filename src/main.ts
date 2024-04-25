8901dad8755ffe773ecfcf9831b8a08e41de6177
Godoneos78 <miguelgalvis821@gmail.com>
2024-04-24 22:09:07 -0400

    Se agrega el Proyecto adicional

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const cors = require('cors');
 
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://meetflowback-production.up.railway.app',
     credentials: true,
   }));
   await app.listen(3001); //Puerto del Backend
